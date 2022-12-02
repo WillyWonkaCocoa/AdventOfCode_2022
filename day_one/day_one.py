0a56b16f157172b732ab985abd4c0eb4f80900bd
Will Gao <willgao@Wills-MacBook-Air.local>
2022-12-02 11:06:15 -0800

    Rename sum variable name. Add logic to keep track of top n elfs with most calories

--- day_one/day_one.py
+++ day_one/day_one.py
@@ -1,20 +1,26 @@
 import sys
+n = 3
 
 if len(sys.argv) < 2:
     raise Exception("Missing input file!")
 else:
     f = open(sys.argv[1], 'r')
     lines = f.readlines()
-    sum = 0
-    highest = [] 
+    total = 0
+    highest = [] #make highest into n+1 sorted array for n highest calories
 
     for line in lines:
         if line.strip():
-            sum = sum + int(line)
-        else:
-            if sum > highest:
-                highest = sum
-            sum = 0
-            
-    print(f'Highest calories is {highest}.')
+            total = total + int(line)
+        else: #add n to sorted array, sort, then delete lowest one
+            if len(highest) < n:
+                highest.append(total)
+            elif len(highest) == n:
+                highest.append(total)
+                highest.sort(reverse=True)
+                highest = highest[:-1]
+            total = 0
+    
+    print(f'The elf carrying the most calories is carrying {highest[0]} calories.')
+    print(f'Highest {n} elfs are carrying {sum(highest)} calories in total.')
     f.close()
