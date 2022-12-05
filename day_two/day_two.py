5ef140154262d05f1404b5e153e0bd1f317831a1
Will Gao <wgao1@umbc.edu>
2022-12-04 22:13:31 -0800

    Add additional constructor logic for part II

--- day_two/day_two.py
+++ day_two/day_two.py
@@ -4,9 +4,9 @@ from enum import Enum
 ABC_Value_Offset = 64
 XYZ_Value_Offset = 23
 
-WIN = 6
 LOSE = 0
 DRAW = 3
+WIN = 6
 
 class Symbol(Enum):
     ROCK = 1
@@ -14,8 +14,34 @@ class Symbol(Enum):
     SCISSOR = 3
 
 class Hand:
-    def __init__(self, hand):
-        self.hand = hand - ABC_Value_Offset
+    def __init__(self, *args):
+        if len(args) > 1:
+            # initalize hand with Symbol based on opponent's hand and desired outcome
+            desired_outcome = args[0] - ABC_Value_Offset
+            opponent_hand = args[1]
+            
+            #desired outcome
+            if desired_outcome == 1: #LOSS
+                if opponent_hand == Symbol.ROCK.value:
+                    self.hand = Symbol.SCISSOR.value
+                elif opponent_hand == Symbol.PAPER.value:
+                    self.hand = Symbol.ROCK.value
+                elif opponent_hand == Symbol.SCISSOR.value:
+                    self.hand = Symbol.PAPER.value
+            elif desired_outcome == 2: #DRAW
+                self.hand = opponent_hand
+            elif desired_outcome == 3: #WIN
+                if opponent_hand == Symbol.ROCK.value:
+                    self.hand = Symbol.PAPER.value
+                elif opponent_hand == Symbol.PAPER.value:
+                    self.hand = Symbol.SCISSOR.value
+                elif opponent_hand == Symbol.SCISSOR.value:
+                    self.hand = Symbol.ROCK.value
+        elif len(args) == 1:
+            self.hand = args[0] - ABC_Value_Offset
+        else:
+            raise Exception("Invalid arg when constructing Hand object")
+        
     def __str__(self):
         if self.hand == Symbol.ROCK.value:
             return "Rock"
@@ -25,6 +51,7 @@ class Hand:
             return "Scissor"
         else:
             return "Invalid hand"
+
     def versus(self, opponent):
         if self.hand == opponent.hand:
             return DRAW
@@ -36,10 +63,6 @@ class Hand:
             return WIN
         else:
             return LOSE
-            
-        
-            
-
 
 if len(sys.argv) < 2:
     raise Exception("Missing input file!")
@@ -51,13 +74,12 @@ else:
     for line in lines:
         opponent, yourself = line.split()
         opponent = Hand(ord(opponent))
-        yourself = Hand(ord(yourself)-XYZ_Value_Offset)
+        yourself = Hand(ord(yourself)-XYZ_Value_Offset, opponent.hand)
         print(f'you get {yourself.hand} for playing {yourself}')
         score += yourself.hand #your decision
         # add score of whether you win, lose, or tie
         score += yourself.versus(opponent)
         
-            
     print(f'Your score following the strategy is {score}.')
     f.close()
 
