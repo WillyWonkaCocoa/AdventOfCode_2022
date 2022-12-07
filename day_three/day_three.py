b0a9c6f980c9f820e610d4dd884bf213fb9f657f
Will Gao <wgao1@umbc.edu>
2022-12-06 23:43:44 -0800

    Update logic for Part II which searches for common char among 3 lines of strings

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -2,6 +2,7 @@ import sys
 
 lower_case_offset = 96
 upper_case_offset = 38
+group_size = 3
 
 if len(sys.argv) < 2:
     raise Exception("Missing input file!")
@@ -9,33 +10,43 @@ else:
     f = open(sys.argv[1], 'r')
     lines = f.readlines()
     sum = 0
+    counter = 0
+    rucksack_dict = {}
     
     for line in lines:
         line = line.strip()
+        #keep track of group of three
+        counter += 1
         
-        # 1. split up rucksack
-        size_of_each_sack = len(line) // 2
-        print(size_of_each_sack)
-        first_rucksack = line[:size_of_each_sack]
-        second_rucksack = line[size_of_each_sack:]
-        print("first rucksack " + first_rucksack)
-        print("second rucksack " + second_rucksack)
-        
-        # 2. search thru rucksack
-        rucksack_dict = {}
-        for n in first_rucksack:
-            rucksack_dict[n] = 1
-        for m in second_rucksack:
-            if m in rucksack_dict.keys():
-                print("The item found in both rucksacks is " + m)
-                priority = ord(m)
-                if priority >= 65 and priority <= 90:
-                    priority -= upper_case_offset
-                else:
-                    priority -= lower_case_offset
-                rucksack_dict.pop(m, None)
-                print("The item found in both rucksacks is " + m + " with a value of " + str(priority))
-                sum += priority
+        if counter == 1:
+        # add all contents to rucksack_dict
+            for n in line:
+                rucksack_dict[n] = 1
+
+        elif counter > 1 :
+        # compare against rucksack_dict
+            temp_rucksack_dict = {}
+            for m in line:
+                if m in rucksack_dict.keys():
+                    temp_rucksack_dict[m] = 1
+            rucksack_dict = temp_rucksack_dict
+            
+            if counter == group_size:
+                #calculate and add value
+                for item in rucksack_dict.keys():
+                    print("The item found in all " + str(group_size) + " rucksacks is " + item)
+                    priority = ord(item)
+                    if priority >= 65 and priority <= 90:
+                        priority -= upper_case_offset
+                    else:
+                        priority -= lower_case_offset
+                    print("The item found in both rucksacks is " + item + " with a value of " + str(priority))
+                    sum += priority
+                
+                
+                #reset counter, rucksack_dict
+                counter = 0
+                rucksack_dict = {}
     
-    print(f'The sum of the priorties of items that appear in both compartments is {sum}.')
+    print(f'The sum of the priorties of items that appear in all {group_size} elf\'s racksack of each group is {sum}.')
     f.close()
