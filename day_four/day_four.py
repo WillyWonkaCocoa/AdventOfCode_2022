e52f50fb51fc630976661bade7f5bfbdf76951bc
Will Gao <wgao1@umbc.edu>
2022-12-07 00:05:40 -0800

    Part I of Day Four, made the mistake of comparing variables when they had not been type converted from strings to ints

---
+++ day_four/day_four.py
@@ -0,0 +1,34 @@
+import sys
+
+partner_separator = ','
+section_separator = '-'
+
+if len(sys.argv) < 2:
+    raise Exception("Missing input file!")
+else:
+    f = open(sys.argv[1], 'r')
+    lines = f.readlines()
+    counter = 0
+
+    for line in lines:
+        line.strip()
+        first, second = line.split(partner_separator)
+        first_begin, first_end = first.split(section_separator)
+        second_begin, second_end = second.split(section_separator)
+        
+        first_begin = int(first_begin)
+        first_end = int(first_end)
+        second_begin = int(second_begin)
+        second_end = int(second_end)
+        
+        if (first_begin >= second_begin and first_end <= second_end) or (second_begin >= first_begin and second_end <= first_end):
+            counter += 1
+        
+    print(f'There are {counter} assignment pairs where one range fully contain the other.')
+    f.close()
+
+
+
+
+ 
+
